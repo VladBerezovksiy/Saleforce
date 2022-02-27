395b90519eef3f69aaf64f0cdfa86f74e1b6d95e
VladBerezovksiy <vladberezovksiy@gmail.com>
2022-02-27 15:13:04 +0200

    Add CapabilitiesGenerator.java

---
+++ src/test/java/utils/CapabilitiesGenerator.java
@@ -0,0 +1,41 @@
+package utils;
+
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.io.File;
+import java.util.HashMap;
+
+public class CapabilitiesGenerator {
+
+    public static final String RESOURCE_PATH = "src/test/resources";
+    public static final File RESOURCE_PATH_FILE = new File(RESOURCE_PATH);
+    public static final String ABSOLUTE_RESOURCE_PATH = RESOURCE_PATH_FILE.getAbsolutePath();
+
+    public static ChromeOptions getChromeOptions() {
+        ChromeOptions options = new ChromeOptions();
+        String driverPath = RESOURCE_PATH;
+        String os = System.getProperty("os.name").toLowerCase();
+        System.out.println("Operational system: " + os + "; Driver path: " + driverPath);
+        if (os.contains("win")) {
+            System.setProperty("webdriver.chrome.driver", driverPath + "/Windows/chromedriver.exe");
+        } else if (os.contains("mac")) {
+            System.setProperty("webdriver.chrome.driver", driverPath + "chromedriver");
+        }
+        options.addArguments("--ignore-certificate-errors");
+        options.addArguments("--disable-popup-blocking");
+        options.addArguments("--disable-notifications");
+        options.addArguments("--headless");
+
+        //options.addArguments("--no-sandbox"); //https://stackoverflow.com/a/50725918/1689770
+        //options.addArguments("--disable-infobars"); //https://stackoverflow.com/a/43840128/1689770
+        //options.addArguments("--disable-dev-shm-usage"); //https://stackoverflow.com/a/50725918/1689770
+        //options.addArguments("--disable-browser-side-navigation"); //https://stackoverflow.com/a/49123152/1689770
+        //options.addArguments("--disable-gpu"); //https://stackoverflow.com/questions/51959986/how-to-solve-selenium-chromedriver-timed-out-receiving-message-from-renderer-exc
+
+        HashMap<String, Object> chromePrefs = new HashMap<String, Object>();
+        chromePrefs.put("download.default_directory", ABSOLUTE_RESOURCE_PATH);
+        options.setExperimentalOption("prefs", chromePrefs);
+        return options;
+    }
+
+}
