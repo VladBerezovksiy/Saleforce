5afac88324592956c26ce8b18e00f91d16fd2295
VladBerezovksiy <vladberezovksiy@gmail.com>
2022-02-28 13:40:05 +0200

    Change CapabilitiesGenerator.java

--- src/test/java/utils/CapabilitiesGenerator.java
+++ src/test/java/utils/CapabilitiesGenerator.java
@@ -7,7 +7,7 @@ import java.util.HashMap;
 
 public class CapabilitiesGenerator {
 
-    public static final String RESOURCE_PATH = "src/test/resources/win/chromedriver.exe";
+    public static final String RESOURCE_PATH = "src/test/resources";
     public static final File RESOURCE_PATH_FILE = new File(RESOURCE_PATH);
     public static final String ABSOLUTE_RESOURCE_PATH = RESOURCE_PATH_FILE.getAbsolutePath();
 
@@ -15,7 +15,11 @@ public class CapabilitiesGenerator {
         ChromeOptions options = new ChromeOptions();
         String os = System.getProperty("os.name").toLowerCase();
         System.out.println("Operational system: " + os + "; Driver path: " + RESOURCE_PATH);
-        System.setProperty("webdriver.chrome.driver", PropertiesUtils.getEnv("driver_path"));
+        if (os.contains("win")) {
+            System.setProperty("webdriver.chrome.driver", PropertiesUtils.getEnv("win_driver_path"));
+        } else if (os.contains("linux")) {
+            System.setProperty("webdriver.chrome.driver", PropertiesUtils.getEnv("linux_driver_path"));
+        }
         options.addArguments("--ignore-certificate-errors");
         options.addArguments("--disable-popup-blocking");
         options.addArguments("--disable-notifications");
