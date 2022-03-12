f7eb2d606446a4ca04f4f8849d2fa1d876327149
VladBerezovksiy <vladberezovksiy@gmail.com>
2022-03-13 00:00:49 +0200

    Change credential

--- src/test/java/utils/CapabilitiesGenerator.java
+++ src/test/java/utils/CapabilitiesGenerator.java
@@ -10,19 +10,14 @@ public class CapabilitiesGenerator {
     public static final String RESOURCE_PATH = "src/test/resources";
     public static final File RESOURCE_PATH_FILE = new File(RESOURCE_PATH);
     public static final String ABSOLUTE_RESOURCE_PATH = RESOURCE_PATH_FILE.getAbsolutePath();
+    private static String driver_path;
 
     public static ChromeOptions getChromeOptions() {
         ChromeOptions options = new ChromeOptions();
         String os = System.getProperty("os.name").toLowerCase();
-        if (os.contains("win")) {
-            String win_driver_path = PropertiesUtils.getEnv("win_driver_path");
-            System.out.println("Operational System: " + os + "; Driver path: " + win_driver_path);
-            System.setProperty("webdriver.chrome.driver", win_driver_path);
-        } else if (os.contains("lin")) {
-            String linux_driver_path = PropertiesUtils.getEnv("linux_driver_path");
-            System.out.println("Operational System: " + os + "; Driver path: " + linux_driver_path);
-            System.setProperty("webdriver.chrome.driver", linux_driver_path);
-        }
+        String driver_path = PropertiesUtils.getEnv("driver_path");
+        System.out.println("Operational System: " + os + "; Driver path: " + driver_path);
+        System.setProperty("webdriver.chrome.driver", driver_path);
         options.addArguments("--ignore-certificate-errors");
         options.addArguments("--disable-popup-blocking");
         options.addArguments("--disable-notifications");
